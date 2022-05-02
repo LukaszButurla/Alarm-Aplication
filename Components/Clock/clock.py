5b45858cb449bebf659310588a73a0d8910a2173
Lukasz Buturla <lukasz.buturla.25@gmail.com>
2022-05-02 17:20:13 +0200

    Add load time function

--- Components/Clock/clock.py
+++ Components/Clock/clock.py
@@ -1,16 +1,30 @@
 from tkinter import Label
+from datetime import date, datetime
 
 class Clock:
 
     frame = None
     clockTxt = None
+    hour = None
+    minute = None
 
     def __init__(self, frame):
 
         self.frame = frame
         self.CreateClock()
+        self.LoadTime()
+        self.ShowTime()
 
     def CreateClock(self):
 
         self.clockTxt = Label(self.frame, text = "00:00", bg = "white", font=("Calibri", 30))
         self.clockTxt.place(x = 100, y = 225, width=100, height=50)
+
+    def LoadTime(self):
+
+        self.hour = datetime.today().hour
+        self.minute = datetime.today().minute
+
+    def ShowTime(self):
+
+        print(self.hour, self.minute)
