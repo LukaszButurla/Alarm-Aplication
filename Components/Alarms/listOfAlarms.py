6e75acc7f18e5fb6779e97947eed9ea5d0359f24
Lukasz Buturla <lukasz.buturla.25@gmail.com>
2022-05-05 16:00:44 +0200

    Add Show alarms function

---
+++ Components/Alarms/listOfAlarms.py
@@ -0,0 +1,49 @@
+import imp
+from tkinter import Label, Button, LabelFrame, PanedWindow
+from tkinter.ttk import LabelFrame
+from Components.Alarms.alarms import Alarms
+from PIL import ImageTk, Image
+
+class ListOfAlarms:
+        
+    frame = None    
+    btnOn = None
+    btnOff = None
+    
+    def __init__(self, frame):
+        
+        self.frame = frame
+        self.btnOn = ImageTk.PhotoImage(Image.open(r"C:\Users\Łukasz\Pictures\Saved Pictures\on-button.png").resize((50, 50)))
+        self.btnOff = ImageTk.PhotoImage(Image.open(r"C:\Users\Łukasz\Pictures\Saved Pictures\off-button.png").resize((50, 50)))
+        self.ShowAlarms()
+        
+    def ShowAlarms(self):
+        
+        print("show")
+        
+        i = 1
+        
+        for a in Alarms.alarmsList:
+            
+            print("Hour: {}".format(a.hour))
+            print("Minute: {}".format(a.minute))
+            
+            labelF = PanedWindow(self.frame, bg = "white")
+            labelF.place(x = 0, y = i * 75, width=300, height= 70)
+            
+            labelHourMinute = Label(labelF, text = "{}:{}".format(a.hour, a.minute), font=("Calibri", 30))
+            labelHourMinute.place(x = 5, y = 0, width=100, height=40, anchor="nw")
+            
+            labelDescription = Label(labelF, text = "{}".format(a.description), font=("Calibri", 10))
+            labelDescription.place(x = 5, y = 45)
+            
+            if a.on == True:                
+            
+                btn = Button(labelF, image = self.btnOn, borderwidth=0, bg = "white")
+                
+            elif a.on == False:
+                
+                btn = Button(labelF, image = self.btnOff, borderwidth=0, bg = "white")
+                
+            btn.place(x = 220, y = 6)
+            i += 1
--- Pages/alarmsPage.py
+++ Pages/alarmsPage.py
@@ -1,22 +1,29 @@
-from tkinter import Frame
+from tkinter import Frame, Label, Spinbox
+from Components.Alarms.listOfAlarms import ListOfAlarms
 
 class AlarmsPage:
 
     window = None
     frame = None
+    listOfAlarms = None
 
     def __init__(self, root):
 
         self.window = root
         self.CreateAlarmsPage()
+        self.listOfAlarms = ListOfAlarms(self.frame)
 
     def CreateAlarmsPage(self):
 
-        self.frame = Frame(self.window, height=490, width=350, bg = "red")
+        self.frame = Frame(self.window, height=490, width=350, bg = "white")
         self.frame.place(x = 0, y = 0)
+        
+        txt = Label(self.frame, text = "Alarms", font=("Calibri", 25), anchor="nw", bg = "white")
+        txt.place(x = 5, y = 5, width=110, height=40)
 
     def ShowAlarmsPage(self):
 
         self.frame.tkraise()
+
 
 
