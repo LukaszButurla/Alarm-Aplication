6ff96292d4febf5df8eeafddd93de5bcf7a09d79
Lukasz Buturla <lukasz.buturla.25@gmail.com>
2022-05-07 13:20:41 +0200

    Add buttons to select color

--- Components/Alarms/AddAlarmPage/colorOfAlarm.py
+++ Components/Alarms/AddAlarmPage/colorOfAlarm.py
@@ -1,5 +1,6 @@
 from Components.Alarms.AddAlarmPage.alarmConfiguration import Configuration
-from tkinter import Frame, Label
+from tkinter import Button, Frame, Label, SUNKEN
+from PIL import Image, ImageTk
 
 
 class ColorOfAlarm:
@@ -7,8 +8,21 @@ class ColorOfAlarm:
     frame = None
     colorFrame = None
     configuration = None
+    blueBtn = None
+    redBtn = None
+    pinkBtn = None
+    orangeBtn = None
+    blueImg = None
+    redImg = None
+    pinkImg = None
+    orangeImg = None
         
     def __init__(self, frame):
+        
+        self.blueImg = ImageTk.PhotoImage(Image.open(r"C:\Users\Łukasz\Pictures\Saved Pictures\blue-circle.png").resize((35, 35)))
+        self.redImg = ImageTk.PhotoImage(Image.open(r"C:\Users\Łukasz\Pictures\Saved Pictures\red-circle.png").resize((35, 35)))
+        self.pinkImg = ImageTk.PhotoImage(Image.open(r"C:\Users\Łukasz\Pictures\Saved Pictures\pink-circle.png").resize((35, 35)))
+        self.orangeImg = ImageTk.PhotoImage(Image.open(r"C:\Users\Łukasz\Pictures\Saved Pictures\orange-circle.png").resize((35, 35)))
         
         self.frame = frame
         self.configuration = Configuration()
@@ -22,3 +36,15 @@ class ColorOfAlarm:
         
         txt = Label(self.colorFrame, text = "Select color:", font=("Calibri", 18) ,bg = "white")
         txt.place(x = 0, y = 0)
+        
+        self.blueBtn = Button(self.colorFrame, image = self.blueImg, borderwidth=0, bg = "white", relief=SUNKEN, activebackground= "white")
+        self.blueBtn.place(x = 40, y = 50, width=37, height=37)
+        
+        self.redBtn = Button(self.colorFrame, image = self.redImg, borderwidth=0, bg = "white", relief=SUNKEN, activebackground= "white")
+        self.redBtn.place(x = 100, y = 50, width=37, height=37)
+        
+        self.pinkBtn = Button(self.colorFrame, image = self.pinkImg, borderwidth=0, bg = "white", relief=SUNKEN, activebackground= "white")
+        self.pinkBtn.place(x = 160, y = 50, width=37, height=37)
+        
+        self.orangeBtn = Button(self.colorFrame, image = self.orangeImg, borderwidth=0, bg = "white", relief=SUNKEN, activebackground= "white")
+        self.orangeBtn.place(x = 220, y = 50, width=37, height=37)
