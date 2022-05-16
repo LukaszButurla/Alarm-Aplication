1bb4d284cfa4828275e28cd93c5c3f351dc86985
DESKTOP-HF35O0S\lukasz <lukasz.buturla.25@gmail.com>
2022-05-16 19:19:15 +0200

    Create call time to call page

--- Components/Alarms/CallPage/alarmInformation.py
+++ Components/Alarms/CallPage/alarmInformation.py
@@ -12,3 +12,6 @@ class AlarmInformation:
                
         hourAndMinute = Label(self.frame, text = "{}:{}".format(alarm.hour, alarm.minute), font=("Calibri", 50), bg = "white")
         hourAndMinute.place(x = 55, y = 75, height=80, width=180)
+        
+        callTIme = Label(self.frame, text = "{}:{}".format(alarm.snoozeHour, alarm.snoozeMinute), font=("Calibri", 35), bg = "white")
+        callTIme.place(x = 75, y = 160, height=60, width=140)
